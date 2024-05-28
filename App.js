4e480662ef51e650b0e9491fab4efb7578b7a87f
Palarnik <maciej.mejer@op.pl>
2024-05-28 23:17:42 +0200

    Add local biometrics

--- App.js
+++ App.js
@@ -1,56 +1,68 @@
-import 'react-native-gesture-handler';
-
-import React, {startTransition, useState} from 'react';
-import Home from './components/Home'
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-import { Alert, Text } from 'react-native';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-
-const Stack = createStackNavigator();
+import React, { useEffect, useState } from 'react';
+import Home from './components/Home';
+import * as LocalAuthentication from 'expo-local-authentication';
+import {Text, View, StyleSheet} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function App() {
-  const navigation = useNavigationContainerRef();
+
+  const [unlocked, setUnlocked] = useState(false);
+
+  useEffect(()=> {
+    if (!unlocked) {
+      authenticate();
+    }
+  },[]);
+
+  const authenticate = async () => {
+    const hasHardware = await LocalAuthentication.hasHardwareAsync();
+
+    if (!hasHardware) {
+      Alert.alert('Biometria nie jest wspierana');
+      return
+    }
+
+    const response = await LocalAuthentication.authenticateAsync();
+    if (response.success) {
+      setUnlocked(true);
+    }
+    
+  }
+
+  if (!unlocked) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.sectionTitle}>Aplikacja zablokowana</Text>
+        <Text style={styles.sectionSubtitle}>Naciśnij kłódkę poniżej, by odblokować treść za pomocą biometrii</Text>
+        <Ionicons onPress = {authenticate} name="lock-closed" size={64} color='#55BCF6'/>
+      </View>
+  );
+  }
 
   return (
-    <NavigationContainer ref={navigation}>
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerMode: 'screen',
-        headerStyle: { backgroundColor: 'transparent' },
-      }}
-    >
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={()=>({
-          title: '',
-          headerRight: ()=> (
-            <FontAwesome.Button name="lock"
-            onPress={() => Alert.alert(
-              'Czy chcesz włączyć szyfrowanie?',
-              'Gdy szyfrowanie zostanie włączone każdorazowo wymagane będzie logowanie PIN-em, lub za pomocą Biometrii',
-              [
-                {text: 'Anuluj', onPress: () => console.log('Cancel Pressed!')},
-                {text: 'Włącz', onPress: ()=> navigation.navigate("Test")},
-              ],
-              { cancelable: false }
-            )}
-            title="Info"
-            color = '#55BCF6'
-            backgroundColor='transparent'
-          />
-          ),
-        })}
-      />
-      <Stack.Screen
-      name="Test"
-      component={Home}
-      />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Home/>
   );
   
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight:'bold',
+    marginBottom: 10
+  },
+  sectionSubtitle: {
+    fontSize: 18,
+    fontWeight:'medium',
+    marginBottom: 40,
+    width: '80%',
+    textAlign:'center',
+    tintColor: '#C0C0C0'
+  }
+});
